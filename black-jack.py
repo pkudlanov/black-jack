9ef469f3db63b95e2c0eca24075e5c5cd09dd467
Pavel Kudlanov <pkudlanov@yahoo.com>
2020-05-02 16:13:36 -0400

    gameplay is done

--- black-jack.py
+++ black-jack.py
@@ -150,14 +150,60 @@ def show_all(player, dealer):
     print("Player's Hand =", player.value)
 
 
-test_deck = Deck()
-test_deck.shuffle()
+while True:
 
-test_player = Hand()
-pulled_card = test_deck.deal()
-print(pulled_card)
-test_player.add_card(pulled_card)
-print(test_player.value)
+    print('WELCOME TO BLACKJACK')
 
-test_player.add_card(test_deck.deal())
-print(test_player.value)
+    deck = Deck()
+    deck.shuffle()
+
+    player_hand = Hand()
+    player_hand.add_card(deck.deal())
+    player_hand.add_card(deck.deal())
+
+    dealer_hand = Hand()
+    dealer_hand.add_card(deck.deal())
+    dealer_hand.add_card(deck.deal())
+
+    player_chips = Chips()
+
+    take_bet(player_chips)
+
+    show_some(player_hand, dealer_hand)
+
+    while playing:
+        hit_or_stand(deck, player_hand)
+
+        show_some(player_hand, dealer_hand)
+
+        if player_hand.value > 21:
+            player_busts(player_hand, dealer_hand, player_chips)
+
+            break
+
+    if player_hand.value <= 21:
+
+        while dealer_hand.value < 17:
+            hit(deck, dealer_hand)
+
+        show_all(player_hand, dealer_hand)
+
+        if dealer_hand.value > 21:
+            dealer_busts(player_hand, dealer_hand, player_chips)
+        elif dealer_hand.value > player_hand.value:
+            dealer_wins(player_hand, dealer_hand, player_chips)
+        elif dealer_hand.value < player_hand.value:
+            player_wins(player_hand, dealer_hand, player_chips)
+        else:
+            push(player_hand, dealer_hand)
+
+    print('\n Player total chips are at: {}'.format(player_chips.total))
+
+    new_game = input('Would you like to play another hand? y/n ')
+
+    if new_game[0].lower() == 'y':
+        playing = True
+        continue
+    else:
+        print('Thank you for playing!')
+        break
